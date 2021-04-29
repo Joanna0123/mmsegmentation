351d841b0ea243e29a281d3adec19b7027188baf
Joanna0123 <81692455+Joanna0123@users.noreply.github.com>
2021-04-29 14:41:31 +0800

    Update pvtconvmodel.py

--- configs/_base_/models/pvtconvmodel.py
+++ configs/_base_/models/pvtconvmodel.py
@@ -16,7 +16,7 @@ model = dict(
         dropout_ratio=0.1,
         num_classes=19,
         norm_cfg=norm_cfg,
-        align_corners=False,
+        align_corners=True,
         loss_decode=dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
     # model training and testing settings
