d9e8565d35f8abf22e5b70ce19e0ff161ab6d638
Joanna0123 <81692455+Joanna0123@users.noreply.github.com>
2021-04-29 14:42:41 +0800

    Update pvt_config.py

--- configs/fcn/pvt_config.py
+++ configs/fcn/pvt_config.py
@@ -1,4 +1,4 @@
 _base_ = [
-    '../_base_/models/pvt.py', '../_base_/datasets/cityscapes_769×769.py',
+    '../_base_/models/pvtconvmodel.py', '../_base_/datasets/cityscapes_769×769.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
